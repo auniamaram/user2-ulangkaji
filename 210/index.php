ad9730df5ddb5e8eac279de41b14e79c075ac9da
Shahadan Bin Saad <shahadansaad@gmail.com>
2019-04-13 10:11:02 +0800

    tambah body

--- 210/index.php
+++ 210/index.php
@@ -1,8 +1,15 @@
 <?php
-$title = "Bismillah";
+	$title = "Bismillah";
 ?>
 <html><head><title>
 <?php
-echo $title;
+	echo $title;
 ?>
 </title></head>
+
+<body>
+	<h1>"Dengan Nama Allah Yang Maha Pemurah lagi Maha Penyayang"</h1>
+
+
+</body>
+</html>
