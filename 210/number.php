efaa7bedd7b3da856242ff52fff2a99ec95e2f6e
Shahadan Bin Saad <shahadansaad@gmail.com>
2019-04-13 10:40:08 +0800

    add file: number.php

---
+++ 210/number.php
@@ -0,0 +1,30 @@
+<html><head><title>Bismillah</title></head>
+
+<body>
+<?
+
+echo 123;
+echo "<br>";
+echo -123.456;
+echo "<br>";
+echo 123 + 456;
+
+$min = -0.1;
+$max = 22;
+$beza = $max - $min;
+echo "<br> Julat = $beza";
+
+$count = 0;
+$count++;
+$count--;
+echo "<br>Hit terkini: ".$count."<br><br>";
+echo abs(-100);
+echo pow(2, 4);
+echo "<br>";
+echo sqrt(25);
+echo "<br>";
+echo round(3.2);
+
+?>
+</body>
+</html>
