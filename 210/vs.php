209601ac7f01a76625ed2f2e6574ed3bc747238f
Shahadan Bin Saad <shahadansaad@gmail.com>
2019-04-13 10:31:44 +0800

    add file: vs.php

---
+++ 210/vs.php
@@ -0,0 +1,12 @@
+<html><head><title>Bismillah</title></head>
+
+<?
+$mula="Dengan Nama Allah Yang Maha Pemurah Lagi Maha Penyayang";
+define("AUTHOR", "Mymie Radzi");
+?>
+<body>
+<h1><?=$mula?></h1> Quadrillion Terima Kasih
+<div align="right">ikhlas dari <?=AUTHOR?></div>
+</body>
+
+</html>
